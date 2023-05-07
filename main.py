1fa029523f97e2d9d9f44a8da0380d5f35dea7a3
Marcos Delay <dev.marcosdly@gmail.com>
2023-05-07 15:57:50 -0300

    added name to video cutting thread

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from video_cut_thread import watch_and_cut
 
 
 def main() -> None:
-    video_cutting_thread = threading.Thread(target=watch_and_cut)
+    video_cutting_thread = threading.Thread(target=watch_and_cut, name="Video_Cutting_Thread")
     video_cutting_thread.start()  # run forever, as it needs to watch a folder
 
 
