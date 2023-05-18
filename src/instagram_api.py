6701fb436dacaf601b5d30adf95354a17893da4c
Marcos Delay <dev.marcosdly@gmail.com>
2023-05-17 21:06:04 -0300

    added instagram_api

---
+++ src/instagram_api.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from instagrapi import Client
+from db import VideoDoc
+from os.path import dirname, join
+from json import load, loads
+
+CREDENTIALS = load(open(join(dirname(__file__), "..", "config", "instagram_login.json"), "rb"))
+SETTINGS_PATH = join(dirname(__file__), "instagram-auth.json")
+
+def login() -> Client:
+    cl = Client()
+    try:
+        cl.load_settings(SETTINGS_PATH)
+    except FileNotFoundError:
+        cl.login(CREDENTIALS["username"], CREDENTIALS["password"], verification_code=CREDENTIALS["2FA"])
+        cl.dump_settings(SETTINGS_PATH)
+    return cl
+
+def post_video_instagram(client: Client, video_path: Path, document: VideoDoc) -> str:
+    media = client.clip_upload(video_path, document.title)
+    url = f"https://www.instagram.com/p/{media.code}"
+    return url
